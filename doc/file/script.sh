56d18df08a3f3fbf8ee310be4b2953c3de8934a9
suisuy <suisuyus@gmail.com>
2022-09-28 12:31:22 +0800

    Update doc/file/script.sh

--- doc/file/script.sh
+++ doc/file/script.sh
@@ -37,7 +37,7 @@ sudo systemctl restart test.service
 
 
 #while.sh
-echo run cmd at case of  every n seconds,useage: frun.sh cmd 
+echo run cmd at case of  every n seconds,useage: frun.sh cmd interval
 
 #_last=${!#}  echo "${@: -1}"
 
