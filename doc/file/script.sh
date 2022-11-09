0d2df3ff44efc3871bc794e553952163a5574f2b
suisuyy <suisuyus@gmail.com>
2022-11-09 22:56:23 +0800

    Update doc/file/script.sh

--- doc/file/script.sh
+++ doc/file/script.sh
@@ -216,9 +216,10 @@ setprop service.adb.tcp.port 5555 ; stop adbd; start adbd
 ```
 
 
-#init.d script put it in /etc/init.d/myinit,
-#sudo ln -s /etc/init.d/myinit /etc/rc3.d/S0myinit # rc3.d may be changed to rc5.d..
-#sudo ln -s /etc/init.d/myinit /etc/rc5.d/S0myinit # rc3.d may be changed to rc5.d..
+#init.d script put it in 
+sudo touch /etc/init.d/myinit,
+sudo ln -s /etc/init.d/myinit /etc/rc3.d/S0myinit # rc3.d may be changed to rc5.d..
+sudo ln -s /etc/init.d/myinit /etc/rc5.d/S0myinit # rc3.d may be changed to rc5.d..
 
 #service "service name" start/stop/status/restart
 ```
