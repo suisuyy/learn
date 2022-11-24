342881becfd94a56f5bdb67a07493c5ca2bd3a6a
suisuyy <suisuyus@gmail.com>
2022-11-24 19:50:47 +0800

    Update doc/file/ubuntusetup.sh

--- doc/file/ubuntusetup.sh
+++ doc/file/ubuntusetup.sh
@@ -1,4 +1,17 @@
 wget https://caddyserver.com/api/download?os=linux&arch=amd64&idempotency=35883637738861
 
-sudo apt -y install python3 htop tinyproxy build-essential git vim unzip unrar screen 
+sudo apt -y install python3 htop tinyproxy build-essential git vim unzip unrar screen fish
+
+
+#config bash and ssh and screen
+echo -e "\n bind 'set completion-ignore-case on'\n bind 'set show-all-if-ambiguous on'\n bind '\C-p:menu-complete'\n" >>~/.bashrc
+echo -e "ServerAliveInterval 1\nServerAliveCountMax 2" >>~/.ssh/config
+
+echo -e \
+"escape ^Jj \n\
+hardstatus alwayslastline\n\
+hardstatus string '%{gk}[%{wk}%?%-Lw%?%{=b kR}(%{W}%n*%f %t%?(%u)%?%{=b kR})%{=w}%?%+Lw%?%? %{g}][%{d}%l%{g}][ %{=w}%Y/%m/%d %0C:%s%a%{g} ]%{W}' \n\
+termcapinfo xterm* ti@:te@"\n\
+>~/.screenrc
+
 
