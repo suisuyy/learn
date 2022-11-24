6eb9f0e7d256f1cffb6cf015992ecb4dc4ae9075
suisuyy <suisuyus@gmail.com>
2022-11-24 20:04:25 +0800

    Update doc/file/ubuntusetup.sh

--- doc/file/ubuntusetup.sh
+++ doc/file/ubuntusetup.sh
@@ -1,6 +1,4 @@
-wget https://caddyserver.com/api/download?os=linux&arch=amd64&idempotency=35883637738861
-
-sudo apt -y install python3 htop tinyproxy build-essential git vim unzip unrar screen fish
+sudo apt -y install python3 htop tinyproxy build-essential git vim unzip unrar screen fish aria2
 
 
 #config bash and ssh and screen
