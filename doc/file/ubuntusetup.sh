c9a03ef41a185f8a016bcf3dd7f459a525546a18
suisuyy <suisuyus@gmail.com>
2022-11-24 19:20:40 +0800

    Update doc/file/ubuntusetup.sh

--- doc/file/ubuntusetup.sh
+++ doc/file/ubuntusetup.sh
@@ -1,1 +1,4 @@
-sudo apt -y install python3 htop tinyproxy build-essential git vim unzip unrar 
+wget https://caddyserver.com/api/download?os=linux&arch=amd64&idempotency=35883637738861
+
+sudo apt -y install python3 htop tinyproxy build-essential git vim unzip unrar screen 
+
