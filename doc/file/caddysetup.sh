f72e74a61a786fe5b6f512d1ae226763963eea4e
suisuyy <suisuyus@gmail.com>
2022-12-14 14:49:17 +0800

    Update doc/file/caddysetup.sh

--- doc/file/caddysetup.sh
+++ doc/file/caddysetup.sh
@@ -2,3 +2,5 @@ sudo apt install curl wget
 
 echo 'deb [trusted=true] https://dl.cloudsmith.io/public/caddy/stable/deb/debian any-version main' >>/etc/apt/source.list
 
+apt update;apt install caddy
+
