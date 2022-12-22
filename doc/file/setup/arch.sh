18f818f3b3058d456a241ed6f0caeac9bd08b180
suisuyy <suisuyus@gmail.com>
2022-12-22 06:24:34 +0000

    Update doc/file/setup/arch.sh

--- doc/file/setup/arch.sh
+++ doc/file/setup/arch.sh
@@ -1,1 +1,13 @@
+#config bash and ssh and screen
+echo -e "\n bind 'set completion-ignore-case on'\n bind 'set show-all-if-ambiguous on'\n bind '\C-p:menu-complete'\n" >>~/.bashrc
+echo -e "ServerAliveInterval 1\nServerAliveCountMax 2" >>~/.ssh/config
+
+
+echo -e \
+"escape ^eE \n\
+hardstatus alwayslastline\n\
+hardstatus string '%{gk}[%{wk}%?%-Lw%?%{=b kR}(%{W}%n*%f %t%?(%u)%?%{=b kR})%{=w}%?%+Lw%?%? %{g}][%{d}%l%{g}][ %{=w}%Y/%m/%d %0C:%s%a%{g} ]%{W}' \n\
+termcapinfo xterm* ti@:te@"\n\
+>~/.screenrc
+
 
