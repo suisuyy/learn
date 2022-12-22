68be3911984b6b8e86f558aa20c47131a1538d57
suisuyy <suisuyus@gmail.com>
2022-12-22 09:50:00 +0000

    Update doc/file/setup/archd.sh

--- doc/file/setup/archd.sh
+++ doc/file/setup/archd.sh
@@ -1,4 +1,4 @@
 
-
+pacman -S xorg-xwininfo
 echo -e "\n[sublime-text]\nSigLevel = Never\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 sudo pacman -Syu sublime-text
