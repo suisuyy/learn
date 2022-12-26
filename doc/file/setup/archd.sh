6932e226976f257506bc6dc3a79c50c438fa659e
suisuyy <suisuyus@gmail.com>
2022-12-26 12:58:23 +0000

    Update doc/file/setup/archd.sh

--- doc/file/setup/archd.sh
+++ doc/file/setup/archd.sh
@@ -1,3 +1,5 @@
+sudo pacman -S wqy-microhei base-devel sudo alsa-utils openssh git vim bash-completion screen w3m aria2 p7zip unrar rsync sshfs tinyproxy ncdu 
+
 echo 'setxkbmap -option caps:swapescape' >>~/.xprofile
 pacman -S xorg-xwininfo
 echo -e "\n[sublime-text]\nSigLevel = Never\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
