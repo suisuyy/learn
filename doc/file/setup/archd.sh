7266b2c722dfb78f3000020337857a134fea9da8
suisuyy <suisuyus@gmail.com>
2023-01-01 08:48:32 +0000

    Update doc/file/setup/archd.sh

--- doc/file/setup/archd.sh
+++ doc/file/setup/archd.sh
@@ -1,6 +1,6 @@
 sudo pacman -S wqy-microhei base-devel sudo alsa-utils openssh git vim bash-completion screen w3m aria2 p7zip unrar rsync sshfs tinyproxy ncdu 
-sudo pacman -S spice-vdagentd 
-pacman -S xorg-xwininfo
+sudo pacman -S spice-vdagentd xorg-xwininfo chromium  
+
 echo -e "\n[sublime-text]\nSigLevel = Never\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 sudo pacman -Syu sublime-text
 
