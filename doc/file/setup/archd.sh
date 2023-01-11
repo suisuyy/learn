4fe324ed302fb3b03425d60be354a1978f780716
suisuyy <suisuyus@gmail.com>
2023-01-11 12:32:43 +0800

    Update doc/file/setup/archd.sh

--- doc/file/setup/archd.sh
+++ doc/file/setup/archd.sh
@@ -10,7 +10,35 @@ sudo pacman -Syu sublime-text
 
 
 #gdm
-cat > /etc/<< "EOF"
+cat >  /etc/gdm3/custom.conf << "EOF"
+# GDM configuration storage
+#
+# See /usr/share/gdm/gdm.schemas for a list of available options.
+
+[daemon]
+# Uncomment the line below to force the login screen to use Xorg
+#WaylandEnable=false
+
+# Enabling automatic login
+#  AutomaticLoginEnable = true
+#  AutomaticLogin = user1
+
+# Enabling timed login
+#  TimedLoginEnable = true
+#  TimedLogin = user1
+#  TimedLoginDelay = 10
+
+[security]
+
+[xdmcp]
+
+[chooser]
+
+[debug]
+# Uncomment the line below to turn on debugging
+# More verbose logs
+# Additionally lets the X server dump core if it crashes
+#Enable=true
 
 
 EOF
