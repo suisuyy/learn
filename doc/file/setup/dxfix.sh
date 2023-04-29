a21c747c609629c31477fcfb6269f5de20d9bccb
suisuyy <suisuyus@gmail.com>
2023-04-29 16:09:10 +0800

    Update doc/file/setup/dxfix.sh

--- doc/file/setup/dxfix.sh
+++ doc/file/setup/dxfix.sh
@@ -1,6 +1,6 @@
 set -x
 
-echo add_drivers+='"ahci megaraid_sas mpt3sas mpt2sas aacraid"   >>'
+echo add_drivers+='"ahci megaraid_sas mpt3sas mpt2sas aacraid"   >>/etc/dracut.conf'
 
 KERNEL_VERSION=`ls /lib/modules/ -1`
 dracut --force --fstab --early-microcode --kmoddir /lib/modules/${KERNEL_VERSION} --kver=${KERNEL_VERSION}
