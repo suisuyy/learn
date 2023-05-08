0420fddf2033f2f90869a418ad5de4531a8222ab
suisuyy <suisuyus@gmail.com>
2023-05-08 22:12:15 +0800

    Update doc/file/setup/arch.sh

--- doc/file/setup/arch.sh
+++ doc/file/setup/arch.sh
@@ -9,7 +9,7 @@ echo -e "\n bind 'set completion-ignore-case on'\n bind 'set show-all-if-ambiguo
 echo -e "ServerAliveInterval 1\nServerAliveCountMax 2" >>~/.ssh/config
 
 echo -e \
-"escape ^eE \n\
+"escape ^aA \n\
 hardstatus alwayslastline\n\
 hardstatus string '%{gk}[%{wk}%?%-Lw%?%{=b kR}(%{W}%n*%f %t%?(%u)%?%{=b kR})%{=w}%?%+Lw%?%? %{g}][%{d}%l%{g}][ %{=w}%Y/%m/%d %0C:%s%a%{g} ]%{W}' \n\
 termcapinfo xterm* ti@:te@"\n\
