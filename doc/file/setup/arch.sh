9d15a2feb0ac1c7e13ee3f32fda1928024df13e8
suisuyy <suisuyus@gmail.com>
2023-05-22 13:10:06 +0800

    Update doc/file/setup/arch.sh

--- doc/file/setup/arch.sh
+++ doc/file/setup/arch.sh
@@ -1,5 +1,14 @@
 
 sudo pacman -S wqy-microhei base-devel sudo openssh usbutils automake autoconf mlocate  alsa-utils  lsof git vim  screen bc nmap w3m aria2 p7zip unrar rsync sshfs tinyproxy ncdu bash-completion qemu-user-static  qemu-user-static-binfmt 
+
+
+#kenel
+cat /proc/sys/vm/swappiness
+echo vm.swappiness=1 >> /etc/sysctl.conf && sysctl -p
+cat /proc/sys/vm/swappiness
+sysctl vm.swappiness
+
+
 #sudo no passwd need
 echo "%suisuy  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 #config bash and ssh and screen 
@@ -12,12 +21,3 @@ hardstatus alwayslastline\n\
 hardstatus string '%{gk}[%{wk}%?%-Lw%?%{=b kR}(%{W}%n*%f %t%?(%u)%?%{=b kR})%{=w}%?%+Lw%?%? %{g}][%{d}%l%{g}][ %{=w}%Y/%m/%d %0C:%s%a%{g} ]%{W}' \n\
 termcapinfo xterm* ti@:te@"\n\
 >~/.screenrc
-
-#kenel
-cat /proc/sys/vm/swappiness
-echo vm.swappiness=1 >> /etc/sysctl.conf && sysctl -p
-cat /proc/sys/vm/swappiness
-sysctl vm.swappiness
-
-
-
