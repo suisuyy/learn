69a1765d19389c71f98ebb7f82b50bdeb5d8091c
suisuyy <suisuyus@gmail.com>
2023-05-22 13:07:31 +0800

    Update doc/file/setup/sinitsetup.sh

--- doc/file/setup/sinitsetup.sh
+++ doc/file/setup/sinitsetup.sh
@@ -1,16 +1,6 @@
-cat >/root/sinit.sh << "EOF"
-#put you command here like
-#/bin/python3 -m http.server --directory /tmp  3003
+export SNAME='sinit'
 
-
-#sleep infinity needed for systemd keep alive,or service status will be loaded not active
-sleep infinity
-
-
-EOF
-
-
-cat >/etc/systemd/system/sinit.service << "EOF"
+cat >/etc/systemd/system/${SNAME}.service << "EOF"
 [Unit]
 Description=auto bash /root/sinit.sh at boot
 
@@ -24,10 +14,36 @@ WantedBy=multi-user.target
 
 EOF
 
-chmod 777 /root/sinit.sh
-chmod 777 /etc/systemd/system/sinit.service
-systemctl enable --now sinit
 
-systemctl restart sinit
-systemctl status --now sinit
+#push your sh in $STARTSH_DIR,$SNAME will exec them all on boot
+
+cat << 'EOF' >/root/${SNAME}.sh 
+export STARTSH_DIR='/root/startsh'
+sleep 10
+
+
+mkdir -p $STARTSH_DIR;
+cd $STARTSH_DIR
+for z in *.sh; do bash $z & done
+
+#put you command here like 
+/bin/screen -S test -dm bash -c "/bin/python3 -m http.server --directory /tmp  3003 ; read;bash" &
+
+sleep infinity
+
+
+EOF
+
+
+
+
+chmod 777 /root/${SNAME}.sh
+chmod 777 /etc/systemd/system/${SNAME}.service
+systemctl enable --now ${SNAME}
+
+#systemctl restart sinit
+systemctl status --now ${SNAME}
+
+
+vim /root/${SNAME}.sh
 
