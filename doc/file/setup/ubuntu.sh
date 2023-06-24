509c203bbdb99082fe44d5de2109236978d8692e
suisuyy <suisuyus@gmail.com>
2023-06-24 14:47:15 +0800

    Update doc/file/setup/ubuntu.sh

--- doc/file/setup/ubuntu.sh
+++ doc/file/setup/ubuntu.sh
@@ -2,7 +2,8 @@
 
 #install packages
 sudo apt update
-sudo apt install -y fonts-wqy-zenhei openssh-server  iputils-ping iperf3 curl wget git vim bash-completion screen htop w3m aria2 p7zip unrar rsync sshfs tinyproxy
+sudo apt install -y apt install -y aria2 w3m tinyproxy
+ openssh-server  iputils-ping iperf3 curl wget git vim bash-completion screen htop w3m aria2 p7zip unrar rsync sshfs tinyproxy
 #sudo apt  install  -y dbus-x11 xfce4  xfce4-terminal ntfs-3g
 
 #config user ,sudo without passwd
