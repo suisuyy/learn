0035babdabeec8e1a7031ad59c735541b4b40de5
suisuyy <suisuyus@gmail.com>
2023-07-05 00:32:46 +0800

    Update doc/file/script.js

--- doc/file/script.js
+++ doc/file/script.js
@@ -28,7 +28,7 @@
 
   //for boadtool bar
   styles = `
-    . { position: fixed; top: 90vh !important; }
+    .chatgptbox-toolbar-container { position: fixed; top: 90vh !important; }
 `;
 
   styleSheet = document.createElement("style");
