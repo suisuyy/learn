58d53a151c5c5025f0387aa6d6d1c18e471eacff
suisuyy <suisuyus@gmail.com>
2023-07-05 00:23:09 +0800

    Update doc/file/script.js

--- doc/file/script.js
+++ doc/file/script.js
@@ -5,7 +5,7 @@
 // @description  try to take over the world!notion.so
 // @author       suisuy
 // @match        https://www.notion.so/*
-// @match        notion.so/*
+// @match        *suisuy*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=notion.so
 // @grant        none
 // ==/UserScript==
